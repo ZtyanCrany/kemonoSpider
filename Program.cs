3934e5abc08af38f0c4c81ed7a8bd785d923f244
ZtyanCrany <1281446464@qq.com>
2023-07-10 15:07:38 +0800

    异步、进度条，显示优化

--- Program.cs
+++ Program.cs
@@ -1,53 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using HtmlAgilityPack;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 using ShellProgressBar;
 
 namespace spider
 {
     class Program
     {
-
-        static void Main(string[] args)
+        private static HttpClient client = new HttpClient();
+        private static long totalFileSize = 0;
+        static async Task Main(string[] args)
         {
             bool carry = true;
             while (carry)
             {
-                Console.Write("Please enter your link(or enter 'E/e' to exit): ");
+                Console.Write("Please enter your link (or enter 'E/e' to exit): ");
                 string url = Console.ReadLine();
-                /*string url = "https://kemono.party/fanbox/user/70050825/post/4256029";*/
 
-                if (url.ToUpper() =="E") { return; }
+                if (url.ToUpper() == "E") 
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("*******\nThanks for using and have a nice day :D\nWriter: ZtyanCrany\n*******\n");
+                    Thread.Sleep(2500);
+                    return;
+                }
                 else
                 {
                     DateTime startTime = DateTime.Now;
-                    downloadGallery(url);
+                    await DownloadSettings(url);
                     DateTime endTime = DateTime.Now;
 
-                    //TODO:异步多线程实现，现在效率极低！
-                    //TODO:窗口化
-                    //TODO:监控进度条
-
                     Console.WriteLine($"All Downloaded.\nTotal time: {Math.Round((endTime - startTime).TotalSeconds, 2)}s.");
+                    Console.WriteLine($"Download number: {index}, Total size: {Math.Round((totalFileSize / (1024.0 * 1024.0)), 2)}MB.\n");
                     Console.ResetColor();
+                    index = 0;
                 }
             }
         }
 
-        static void downloadGallery(string url)
+        static async Task DownloadSettings(string url)
         {
             string htmlContent;
             string pagePath = "//*[@id=\"page\"]/";
 
-            WebClient client = new WebClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+
+            htmlContent = await client.GetStringAsync(url);
             HtmlDocument document = new HtmlDocument();
-            client.Encoding = Encoding.UTF8; //解决乱码
-            htmlContent = client.DownloadString(url);
             document.LoadHtml(htmlContent);
 
             string galleryPath = pagePath + "div/div";
@@ -59,58 +65,86 @@ namespace spider
             HtmlNode topicNode = document.DocumentNode.SelectSingleNode(topicPath);
             string authorText = authorNode.InnerText.Trim();
             string topicText = topicNode.InnerText.Trim();
-            Console.WriteLine($"Author:{authorText}");
-            Console.WriteLine($"Topic:{topicText}");
+            Console.ForegroundColor=ConsoleColor.Cyan;
+            Console.WriteLine($"Author: {authorText}");
+            Console.WriteLine($"Topic: {topicText}");
+            Console.ResetColor();
 
-            //create file
             if (topicText.Contains("/"))
             {
                 topicText = topicText.Replace("/", "_");
             }
             string createFilePath = $@"{Directory.GetCurrentDirectory()}/Download/【{authorText}】{topicText}";
             Directory.CreateDirectory(createFilePath);
-            /*Console.WriteLine(createFilePath);*/
 
-            foreach (HtmlNode divNode in divNodes)
+            var progressBarOptions = new ProgressBarOptions
             {
-                HtmlNodeCollection hrefNodes = divNode.SelectNodes(".//a[@href]");
-                if (hrefNodes != null)
+                ProgressCharacter = '▇',
+                ProgressBarOnBottom = true
+            };
+
+            using (var progressBar = new ProgressBar(divNodes.Count, "Downloading...", progressBarOptions))
+            {
+                var tasks = new List<Task>();
+
+                foreach (HtmlNode divNode in divNodes)
                 {
-                    int index = 1;
-                    foreach (HtmlNode hrefNode in hrefNodes)
+                    HtmlNodeCollection hrefNodes = divNode.SelectNodes(".//a[@href]");
+                    if (hrefNodes != null)
                     {
-                        string imageUrl = hrefNode.GetAttributeValue("href", "");
-                        Console.ResetColor();
-                        Console.WriteLine(imageUrl);
-                        try
+                        foreach (HtmlNode hrefNode in hrefNodes)
                         {
-                            string fileName = Path.GetFileName(imageUrl).Split(new char[] { '.' })[^1];
-                            string filePath = $"{createFilePath}/Art-{index}" + '.' + fileName;
+                            string imageUrl = hrefNode.GetAttributeValue("href", "");
+                            Console.ResetColor();
+                            Console.WriteLine(imageUrl);
 
-                            //进度条显示
-                            
-
-                            client.DownloadFile(imageUrl, filePath);
-
-                            FileInfo fileInfo = new FileInfo(filePath);
-                            double fileSizeInMB = 1.0;
-                            if (fileInfo.Exists)
-                            {
-                                long fileSizeInB = fileInfo.Length;
-                                fileSizeInMB = fileSizeInB / 1024.0 / 1024.0;
-                            }
-
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Art-{index++}:Download already!");
-                            Console.WriteLine($"ImageSize: {Math.Round(fileSizeInMB, 2)}MB");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Download Error! " + ex.Message);
+                            tasks.Add(DownloadGallery(imageUrl, createFilePath, progressBar));
                         }
                     }
                 }
+                await Task.WhenAll(tasks);
+                progressBar.Message = "Download complete!";
+            }
+        }
+
+        static int index = 0;
+        static async Task DownloadGallery(string imageUrl, string createFilePath, ProgressBar progressBar)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(imageUrl).Split(new char[] { '.' })[^1];
+                string filePath = $"{createFilePath}/Art-{++index}" + '.' + fileName;
+                Thread.Sleep(10);
+
+                using (var response = await client.GetAsync(imageUrl, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        using (var fileStream = File.Create(filePath))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+                    }
+                }
+
+                //Calculate downloads
+                FileInfo fileInfo = new FileInfo(filePath);
+                if (fileInfo.Exists)
+                {
+                    long fileSizeInB = fileInfo.Length;
+                    Interlocked.Add(ref totalFileSize, fileSizeInB);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Downloading... Art-{index}");
+                progressBar.Tick();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Download Error! " + ex.Message);
             }
         }
     }
